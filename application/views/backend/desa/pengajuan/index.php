34d70609666cb05de4ee31d8ff5398a16e1f3763
Dirga Yasa <cekingx@gmail.com>
2020-11-02 11:52:11 +0800

    quickfix

--- application/views/backend/desa/pengajuan/index.php
+++ application/views/backend/desa/pengajuan/index.php
@@ -147,9 +147,6 @@ var KTDatatableRemoteAjaxDemo = function() {
             }, {
                 field: 'pengajuan_nik',
                 title: 'NIK Pemohon',
-                template: function(row) {
-                    return '<a href="<?= base_url('desa/pengajuan/show/') ?>' +  row.pengajuan_id + '">' + row.pengajuan_nik + '</a>'
-                }
             }, {
                 field: 'pengajuan_created_at',
                 title: 'Tanggal Pengajuan',
@@ -173,7 +170,29 @@ var KTDatatableRemoteAjaxDemo = function() {
                     return '<span class="label label-primary label-dot mr-2"></span><span class="font-weight-bold text-primary">' +
                         row.layanan + '</span>';
                 },
-            }],
+            }, {
+                field: 'Actions',
+                title: 'Actions',
+                sortable: false,
+                width: 100,
+                overflow: 'visible',
+                autoHide: false,
+                template: function(row) {
+                    return `
+                        <button data-id="${row.pengajuan_id}" class="btn btn-clean mr-2 row text-primary bg-primary-o-30 btnShow" title="Show details">
+                            <span class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2020-10-29-133027/theme/html/demo1/dist/../src/media/svg/icons/General/Visible.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <rect x="0" y="0" width="24" height="24"/>
+                                    <path d="M3,12 C3,12 5.45454545,6 12,6 C16.9090909,6 21,12 21,12 C21,12 16.9090909,18 12,18 C5.45454545,18 3,12 3,12 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"/>
+                                    <path d="M12,15 C10.3431458,15 9,13.6568542 9,12 C9,10.3431458 10.3431458,9 12,9 C13.6568542,9 15,10.3431458 15,12 C15,13.6568542 13.6568542,15 12,15 Z" fill="#000000" opacity="0.3"/>
+                                </g>
+                            </svg><!--end::Svg Icon--></span>
+                            Lihat
+                        </button>
+                    `;
+                }
+            }
+            ],
 
         });
 
@@ -186,6 +205,12 @@ var KTDatatableRemoteAjaxDemo = function() {
         });
 
         $('#kt_datatable_search_status, #kt_datatable_search_type').selectpicker();
+
+        $(document).on("click", ".btnShow", function() {
+            let id = $(this).data('id');
+            
+            window.location.replace("<?= base_url('desa/pengajuan/show/') ?>" + id);
+        });
     };
 
     return {
